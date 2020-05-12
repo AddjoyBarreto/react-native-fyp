4a41ddb4a1cd0899b7058180a21011bd6e760402
unknown <vedang55.vd@gmail.com>
2020-05-12 09:32:50 +0530

    display bookmarks

--- screens/Bookmark.js
+++ screens/Bookmark.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View,Text, Dimensions, ScrollView,TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, Dimensions, ScrollView, TouchableOpacity } from 'react-native';
 import { EvilIcons } from '@expo/vector-icons';
 import GateStatus from '../components/GateStatus';
 import { DrawerActions } from 'react-navigation-drawer';
@@ -9,88 +9,98 @@ const { width, height } = Dimensions.get('window');
 const arr = []
 class Bookmark extends Component {
 
-    state = {
-      tableHead: ['Train', 'Time'],
-      tableData: [
-        ['Train', '8:45'],
-        ['Train', '9:45'],
-        ['Train', '10:50'],
-        ['Train', '12:30']
-      ],
-      toggleArrow: false,
-      array:[]
-    }
+  state = {
+    tableHead: ['Train', 'Time'],
+    tableData: [
+      ['Train', '8:45'],
+      ['Train', '9:45'],
+      ['Train', '10:50'],
+      ['Train', '12:30']
+    ],
+    toggleArrow: false,
+    array: []
+  }
 
-constructor(props){
-  super(props)
-  let userid = firebase.auth().currentUser.uid
-  try{
-    firebase.database().ref('users/' + userid+'/bookmark').once('value', function(snapshot) {
-      console.log('bookmarked in DB',Object.values(snapshot.val()));
-      //arr = Object.values(snapshot.val())
-      this.setState({array: Object.values(snapshot.val())})
-      console.log(this.state.array)
-    });
+
+  componentDidMount() {
+    let userid = firebase.auth().currentUser.uid
+    try {
+      firebase.database().ref('users/' + userid + '/bookmark').on('value', async (snapshot) => {
+        console.log('bookmarked in DB', Object.values(snapshot.val()));
+        //arr = Object.values(snapshot.val())
+        const bookmarkStationNames = Object.values(snapshot.val())
+        const bookmarkStationObjects = []
+        console.log(bookmarkStationNames, 'statio names')
+        for (stationName of bookmarkStationNames) {
+          const stationObj = (await firebase.database().ref('/stations/').orderByChild('name').equalTo(stationName).once("value")).val().filter(item => !!item)[0]
+          bookmarkStationObjects.push(stationObj)
+          console.log(stationObj, 'stationobj')
+        }
+        console.log(bookmarkStationObjects)
+        this.setState({ array: bookmarkStationObjects })
+        console.log(this.state.array)
+      });
+    }
+    catch (err) {
+      console.log(err)
+    }
   }
-  catch(err){
-    console.log(err)
-  }
-}
+
 
   onArrowClick = (id) => {
-    console.log(id);  
-      this.setState({ toggleArrow: !this.state.toggleArrow})
+    console.log(id);
+    this.setState({ toggleArrow: !this.state.toggleArrow })
   }
 
-        
-    
-render(){
-        const mapsbyid = [{
-          id:1,
-          gatename:'Majorda',
-          
-        },
-        {
-          id:2,
-          gatename:'Utorda',
-          
-        },
-      ]
 
-    return(
-       
-        <View style={styles.screen}>
-        <TouchableOpacity style={{marginTop:20,width:width}} onPress={() => {
-            this.props.navigation.dispatch(DrawerActions.toggleDrawer());
-          }}>
-            <EvilIcons name="navicon" size={40} color="black" />
-          </TouchableOpacity>
-          <ScrollView>
 
-            { this.state.array.map(mapobj=>{
-              //console.log(mapobj.id); 
-              return (<React.Fragment key={mapobj.id}>
-                <GateStatus onArrowClick={()=>this.onArrowClick(mapobj.id)} 
-                                  toggleArrow={this.state.toggleArrow} 
-                                  state={this.state}
-                                  mapobj={mapobj} />
-                </React.Fragment>)
-            }) }
-                       
-          </ScrollView>                         
-        </View>
-        
+  render() {
+    const mapsbyid = [{
+      id: 1,
+      gatename: 'Majorda',
+
+    },
+    {
+      id: 2,
+      gatename: 'Utorda',
+
+    },
+    ]
+
+    return (
+
+      <View style={styles.screen}>
+        <TouchableOpacity style={{ marginTop: 20, width: width }} onPress={() => {
+          this.props.navigation.dispatch(DrawerActions.toggleDrawer());
+        }}>
+          <EvilIcons name="navicon" size={40} color="black" />
+        </TouchableOpacity>
+        <ScrollView>
+
+          {this.state.array.map(mapobj => {
+            //console.log(mapobj.id); 
+            return (<React.Fragment key={mapobj.name}>
+              <GateStatus onArrowClick={() => this.onArrowClick(mapobj.id)}
+                toggleArrow={this.state.toggleArrow}
+                state={this.state}
+                mapobj={mapobj} />
+            </React.Fragment>)
+          })}
+
+        </ScrollView>
+      </View>
+
     )
-}
+  }
 }
 
 export default Bookmark;
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'space-between',
-        marginTop:height*0.01,
-    },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: height * 0.01,
+  },
 })
